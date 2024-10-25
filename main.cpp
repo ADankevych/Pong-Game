b99fa2c62f76d8377c46d2d7a7f2da7550130a27
adankevych <adankevych@kse.org.ua>
2024-10-25 22:02:04 +0300

    Added first page, and "Welcome" message

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <SFML/Graphics.hpp>
-
 using namespace std;
 using namespace sf;
 
@@ -14,14 +13,30 @@ int main()
     {
         Event event{};
 
-
         while (window.pollEvent(event))
         {
             if (event.type == Event::Closed)
                 window.close();
         }
 
-        window.clear();
+        window.clear(Color(190, 190, 190));
+        Text text;
+        Font font;
+        font.loadFromFile("/Users/anastasia_d/CLionProjects/asmt-5-game-engine-ADankevych/times-new-roman.ttf");
+        text.setFont(font);
+        string textMessage = "Welcome to Pong game!";
+        text.setString(textMessage);
+        text.setCharacterSize(40);
+        text.setFillColor(Color::Black);
+        text.setPosition((BOARD_WIDTH - text.getLocalBounds().width)/2, BOARD_HEIGHT/3); // text.getLocalBounds().width - width of the text
+        window.draw(text);
+
+        textMessage = "Choose one option:";
+        text.setString(textMessage);
+        text.setCharacterSize(30);
+        text.setPosition((BOARD_WIDTH - text.getLocalBounds().width)/2, BOARD_HEIGHT/3 + 100);
+        window.draw(text);
+
         window.display();
     }
 }
