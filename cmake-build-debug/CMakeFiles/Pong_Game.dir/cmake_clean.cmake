028a6825654986332e0754643657b3936e2290a6
adankevych <adankevych@kse.org.ua>
2024-11-02 12:44:22 +0200

    project files

---
+++ cmake-build-debug/CMakeFiles/Pong_Game.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pong_Game.dir/main.cpp.o"
+  "CMakeFiles/Pong_Game.dir/main.cpp.o.d"
+  "Pong_Game"
+  "Pong_Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pong_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
