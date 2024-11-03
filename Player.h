642672409752e3584fc4e37301b78023e333f34e
adankevych <adankevych@kse.org.ua>
2024-11-03 18:12:58 +0200

    Class Player

--- Player.h
+++ Player.h
@@ -4,5 +4,35 @@
 
 #ifndef PONG_GAME_PLAYER_H
 #define PONG_GAME_PLAYER_H
+#include "Paddle.h"
 
+class Player {
+private:
+    string name;
+    Paddle paddle;
+    int score;
+
+public:
+    Player() = default;
+
+    Player(string name, Paddle paddle) {
+        this->name = name;
+        this->paddle = paddle;
+        this->score = 0;
+    }
+
+    void draw(RenderWindow &window) {
+        paddle.draw(window);
+    }
+
+    void moveUp() {
+        paddle.moveUp();
+    }
+
+    void moveDown() {
+        paddle.moveDown();
+    }
+
+
+};
 #endif //PONG_GAME_PLAYER_H
