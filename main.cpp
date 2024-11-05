7bed676a28f0daa63b274a2c37d8e7ab7236e03f
adankevych <adankevych@kse.org.ua>
2024-11-05 12:01:11 +0200

    Replace code with startNewGame method

--- main.cpp
+++ main.cpp
@@ -1,37 +1,11 @@
 #include "GameSettings.h"
 #include "GameProcess.h"
 
-void gameProcessFriends(RenderWindow &window) {
-    while (window.isOpen()) {
-        window.clear(Color(190, 190, 190));
-        window.display();
-    }
-}
-
-void gameProcessBot(RenderWindow &window) {
-    while (window.isOpen()) {
-        window.clear(Color(190, 190, 190));
-        window.display();
-    }
-}
-
-
 int main() {
     RenderWindow window(VideoMode(BOARD_WIDTH, BOARD_HEIGHT), "Pong");
     GameProcess gameProcess;
 
-    int playWithBorF = GameSettings::welcomePage(window);
-    gameProcess.setModeAndDifficulty(1, 1);
+    gameProcess.startNewGame(window);
 
-    if (playWithBorF == 1) {
-        gameProcess.setNamePlayer1(GameSettings::nameEntering(window, "Enter your name:"));
-        gameProcess.setNamePlayer2("Bot");
-    } else if (playWithBorF == 2) {
-        gameProcess.setNamePlayer1(GameSettings::nameEntering(window, "Enter player 1 name:"));
-        gameProcess.setNamePlayer2(GameSettings::nameEntering(window, "Enter player 2 name:"));
-    }
-
-    gameProcess.setModeAndDifficulty(GameSettings::chooseDifficulty(window), GameSettings::chooseMode(window));
-    gameProcess.draw(window);
     return 0;
 }
